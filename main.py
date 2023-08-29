932b8b5b77e067d89afc3957b8c0b13794479d2e
Krenare <egzonixbb@gmail.com>
2023-08-29 13:36:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import math
+var_acceleration = float(input ("value of acceleration"))
+var_mass = int(input("mass of object"))
+var_gravity = (var_acceleration * var_mass)
+print("weight of the object is", var_gravity)
